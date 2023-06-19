01c80f4574b768568af56300094591308ab60724
soteliin <soteliin5@gmail.com>
2023-06-19 16:31:05 -0600

    problemas con graficas

--- src/app/components/graficas/graficas.component.ts
+++ src/app/components/graficas/graficas.component.ts
@@ -10,52 +10,20 @@ import { UserServiceService } from 'src/app/services/user-service.service';
   templateUrl: './graficas.component.html',
   styleUrls: ['./graficas.component.css']
 })
-export class GraficasComponent implements OnInit {
-  chart: | undefined;
-  correo: string = "";
-  constructor(private router: Router) {
-
-  }
-  ngOnInit(): void {
-
-
-    this.correo = localStorage.getItem("correo") || "";
-    if (this.correo == "")
-      this.router.navigate(['/login']);
-    Chart.register(BarController, LinearScale, CategoryScale);
-    const barChart = new Chart('barChart', {
-      type: 'bar',
-      data: {
-        labels: ['Villa Completa en Punta Cana', 'Hotel Majestic-Barcelona', 'Casa Ecológica en Groenlandia', 'Casa frente al mar en Cancún',
-          'Villa en Mazamitla', 'Villa completa en Vallarta'],
-        datasets: [
-          { data: [45, 49, 50, 41, 30, 32], label: 'Número de Reservaciones', backgroundColor: 'green', hoverBackgroundColor: 'purple' },
-        ]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          y: {
-            type: 'linear',
-            beginAtZero: true
-          }
-        }
-      }
-    });
-  }
-
-  Texto = "Gráfica de Barras";
-  //Texto: segundo = "Hospedajes más reservados";
-
-  Fecha: Date = new Date();
 export class GraficasComponent implements OnInit{
   
   //se inyecta el servicio en el constructor
-  constructor(private userService: UserServiceService) {}
+  constructor(private userService: UserServiceService,private router: Router) {}
+  
+  correo: string = "";
 
 
    chart:  | undefined;
     ngOnInit(): void {
+      
+    this.correo = localStorage.getItem("correo") || "";
+    if (this.correo == "")
+      this.router.navigate(['/login']);
       //se obtienen los datos de la base de datos con el método getCita() del servicio
       this.userService.getCita().subscribe(citas => {
         //obtenemos el titulo de las casas reservadas y el número de huespedes
