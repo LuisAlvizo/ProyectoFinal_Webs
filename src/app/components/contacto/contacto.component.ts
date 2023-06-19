7768f92b2b3f914730465cbd16453451e27b5549
Gonzalo263614 <gonzalo_ruso19@outlook.com>
2023-06-18 21:13:38 -0600

    Termine

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -20,19 +20,23 @@ export class ContactoComponent {
       mensaje:new FormControl('',Validators.required),
     })
   }
-
+  
   envioCorreo(){
     if(this.datos.valid){
       this.resultado = "Todos los datos son válidos";
       let params = {
-        email:this.datos.value.correo,
+        //email:this.datos.value.correo,
+        email: "ruvalcabasolisgonzalo19@gmail.com", //CORREO ADMINISTRADOR
         asunto:this.datos.value.asunto,
-        mensaje:this.datos.value.mensaje
+        //mensaje:this.datos.value.mensaje
+        mensaje: "La persona con nombre: "+this.datos.value.nombre+" y correo: "+this.datos.value.correo+" a utilizado su formulario. \nPregunta: "+this.datos.value.mensaje
       }
       console.log(params)
       this.httpclient.post('https://apicorreo.onrender.com/envio',params).subscribe(resp=>{
         console.log(resp);
       });
+      //var campo = document.getElementById('nombre') as HTMLInputElement;
+      //campo.value = ''; // Establecer el valor del campo como una cadena vacía
     }else{
       this.resultado = "Hay datos inválidos en el formulario";
     }
